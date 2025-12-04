3b443d523e3631a84afc67cacfad641384944a44
Hemen Ali <hemen.ali@lansforsakringar.se>
2025-12-04 09:39:45 +0100

    updated bundle paths

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+const base = process.env.VITE_BASE || "/newrozff/";
 
 export default defineConfig({
   plugins: [react()],
-  base: '/newrozff/',
-})
+  base,
+});
